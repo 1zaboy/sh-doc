1e3e987ecd9e158a5080f69248e3f42a8d3a7859
Vlad <33203144+1zaboy@users.noreply.github.com>
2024-10-02 13:52:45 +0300

    fix

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -12,16 +12,16 @@ const config: Config = {
   deploymentBranch: 'main',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://1zaboy.github.io/sh-doc/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  projectName: 'docusaurus', // Usually your repo name.
+  projectName: 'sh-doc', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
